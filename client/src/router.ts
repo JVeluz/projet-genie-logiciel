c67d15fda056331d0e59751d08316c39dbc1b852
JVeluz <j.veluz2002@gmail.com>
2025-10-29 21:05:51 +0100

    gitignore

--- client/src/router.ts
+++ client/src/router.ts
@@ -23,7 +23,6 @@ export default function navigateTo(fullPath: string): void {
 
 window.onpopstate = () => renderCurrentPage();
 
-
 const initialPath = window.location.pathname + window.location.search;
 history.replaceState(null, "", initialPath);
 renderCurrentPage();
