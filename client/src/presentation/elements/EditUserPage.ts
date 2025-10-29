1f858fb9dd6de2d6b9279fd82b88e85eb496da62
JVeluz <j.veluz2002@gmail.com>
2025-10-29 18:39:39 +0100

    UserEditor

---
+++ client/src/presentation/elements/EditUserPage.ts
@@ -0,0 +1,19 @@
+import HTML from "@html/user-edit-page.html";
+import UserElement from "./UserElement";
+import ApplicationModel from "@core/ApplicationModel";
+import User from "@models/User";
+
+
+export default class EditUserPage extends HTMLElement {
+
+    public connectedCallback(): void {
+        this.innerHTML = HTML;
+        const currentUser: Object | null = ApplicationModel.getInstance().get("currentUser");
+        if (currentUser) {
+            const userElement = this.querySelector('user-element') as UserElement;
+            customElements.whenDefined('user-element').then(() => {
+                userElement.update(currentUser as User);
+            });
+        }
+    }
+}
