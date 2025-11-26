ec6fea57deaf9b3ef056fec8bf18f6efc3980f91
JVeluz <150319357+JVeluz@users.noreply.github.com>
2025-11-26 11:10:10 +0100

    Delete server/public directory

--- server/public/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = errorHandler;
-function errorHandler(error, request, response, next) {
-    console.error("💥 ERRROR :", error.message);
-    return response.status(500).json({
-        message: error.message
-    });
-}
